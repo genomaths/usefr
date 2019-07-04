61daae0a45d1252b8b796845c923775a1e647a04
genomaths <18340747+genomaths@users.noreply.github.com>
2019-07-04 19:16:37 -0400

    Add new functions gamma3p

--- R/gamma3p.R
+++ R/gamma3p.R
@@ -17,22 +17,22 @@
 ## You should have received a copy of the GNU General Public License along with
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
-#' @rdname weibull3p
-#' @name weibull3p
-#' @aliases weibull3p
-#' @aliases rweibull3p
-#' @aliases pweibull3p
-#' @aliases dweibull3p
-#' @aliases qweibull3p
+#' @rdname gamma3p
+#' @name gamma3p
+#' @aliases gamma3p
+#' @aliases rgamma3p
+#' @aliases pgamma3p
+#' @aliases dgamma3p
+#' @aliases qgamma3p
 #'
-#' @title Weibull Distribution with Three Parameters
+#' @title Gamma Distribution with Three Parameters
 #' @description Probability density function (PDF), cummulative density function
 #'     (CDF), quantile function and random generation for the Three-Parameter
-#'     Weibull (weibull3p) distribution.
-#' @details It is important to mention that Weibull distribution is defined for
+#'     Gamma (gamma3p) distribution.
+#' @details It is important to mention that Gamma distribution is defined for
 #'     \eqn{x > 0} and, therefore, values \eqn{x < 0} will yield probability
 #'     zero. While to obtain negative random values with function
-#'     \strong{rweibull3p} is an indication that the location parameter
+#'     \strong{rgamma3p} is an indication that the location parameter
 #'     \eqn{\mu} has not a valid value.
 #' @param x,q numeric vector
 #' @param n number of observations
@@ -43,67 +43,67 @@
 #'     otherwise, P[X > x]
 #' @param log.p logical; if TRUE, probabilities/densities p are returned as
 #'     log(p).
-#' @return Three-parameter PDF values for dweibull3p, three-parameter
-#'     probability for pweibull3p, quantiles or three-parameter random
-#'     generated values for rweibull3p.
+#' @return Three-parameter PDF values for dgamma3p, three-parameter
+#'     probability for pgamma3p, quantiles or three-parameter random
+#'     generated values for rgamma3p.
 #' @examples
 #' set.seed(123) # set a seed for random generation
-#' ##  Sampling from the specified Weibull distribution
-#' r <- rweibull3p(n = 1e5, shape = 1.4, scale = 3.7, mu = 0.9)
+#' ##  Sampling from the specified Gamma distribution
+#' r <- rgamma3p(n = 1e5, shape = 1.4, scale = 3.7, mu = 0.9)
 #'
 #' ## The histogram and density
 #' hist(r, 100, freq = FALSE, xlim = c(0, 20))
 #' r1 <- seq(0, 20, by = 0.1)
-#' lines(r1, dweibull3p(r1, shape = 1.4, scale = 3.7, mu = 0.9),
+#' lines(r1, dgamma3p(r1, shape = 1.4, scale = 3.7, mu = 0.9),
 #'     col = "red")
 #'
 
-#' @name dweibull3p
-#' @rdname weibull3p
-#' @title Weibull Distribution with Three Parameters
+#' @name dgamma3p
+#' @rdname gamma3p
+#' @title Gamma Distribution with Three Parameters
 #' @description NULL
 #' @details NULL
 #' @export
-dweibull3p <- function(x, shape = 1, scale = 1, mu = 0, log = FALSE) {
-  d <- dweibull(x = x - mu, shape = shape, scale = scale, log = log)
+dgamma3p <- function(x, shape = 1, scale = 1, mu = 0, log = FALSE) {
+  d <- dgamma(x = x - mu, shape = shape, scale = scale, log = log)
   return(d)
 }
 
 
-#' @name pweibull3p
-#' @rdname weibull3p
-#' @title Weibull Distribution with Three Parameters
+#' @name pgamma3p
+#' @rdname gamma3p
+#' @title Gamma Distribution with Three Parameters
 #' @description NULL
 #' @details NULL
 #' @export
-pweibull3p <- function(q, shape = 1, scale = 1, mu = 0, lower.tail = TRUE,
+pgamma3p <- function(q, shape = 1, scale = 1, mu = 0, lower.tail = TRUE,
                       log.p = FALSE) {
-   p <- pweibull(q = q - mu, shape = shape, scale = scale,
+   p <- pgamma(q = q - mu, shape = shape, scale = scale,
                    lower.tail = lower.tail, log.p = log.p)
    return(p)
 }
 
-#' @name qweibull3p
-#' @rdname weibull3p
-#' @title Weibull Distribution with Three Parameters
+#' @name qgamma3p
+#' @rdname gamma3p
+#' @title Gamma Distribution with Three Parameters
 #' @description NULL
 #' @details NULL
 #' @export
-qweibull3p <- function(p, shape = 1, scale = 1, mu = 0, lower.tail = TRUE,
+qgamma3p <- function(p, shape = 1, scale = 1, mu = 0, lower.tail = TRUE,
                       log.p = FALSE) {
-   q <- qweibull(p = p, shape = shape, scale = scale, lower.tail = lower.tail,
+   q <- qgamma(p = p, shape = shape, scale = scale, lower.tail = lower.tail,
                  log.p = log.p) + mu
    return(q)
 }
 
-#' @name rweibull3p
-#' @rdname weibull3p
-#' @title Weibull Distribution with Three Parameters
+#' @name rgamma3p
+#' @rdname gamma3p
+#' @title Gamma Distribution with Three Parameters
 #' @description NULL
 #' @details NULL
 #' @export
-rweibull3p <- function(n, shape = 1, scale = 1, mu = 0) {
-   r <- rweibull(n = n, shape = shape, scale = scale) + mu
+rgamma3p <- function(n, shape = 1, scale = 1, mu = 0) {
+   r <- rgamma(n = n, shape = shape, scale = scale) + mu
    return(r)
 }
 
