e18ff177a8ee914519f8b659bf5e4d67ee160771
genomaths <18340747+genomaths@users.noreply.github.com>
2020-10-22 22:14:46 -0400

    Add new function nlmR

---
+++ R/nlmR.R
@@ -0,0 +1,55 @@
+## Copyright (C) 2019 Robersy Sanchez <https://genomaths.com/>
+##
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
+##
+#' @rdname nlmR
+#' @title Nonlinear Goodness-of-fit and Determination Coefficients
+#' @description This function returns goodness-of-fit criteria for nonlinear
+#' model selection.
+#' @details Adjusted R squared (rho), the AIC, and BIC are estimated. The
+#' Stein's formula for adjusted R squared (rho) is applied as an estimator of
+#' the average cross-validation predictive power (1).
+#' @param object A 'nls' object, typically returned after performing a
+#' nonlinear regression fit with function \code{\link[stats]{nls}} or
+#' \code{\link[minpack.lm]{nls.lm}} or \code{\link[minpack.lm]{nlsLM}}.
+#' @export
+#' @references
+#' 1. Stevens JP. Applied Multivariate Statistics for the Social
+#'     Sciences. Fifth Edit. Routledge Academic; 2009.
+#' @author Robersy Sanchez - 10/22/2020
+
+nlmR <- function(object) {
+     if (!inherits(object, "nls"))
+          stop("\n*** 'object' must inherits from class 'nls', i.e.,\n",
+          "an object derived from nonlinear fit from fron functions: \n",
+          "'nls' or 'nlsLM' or 'nls.ml' from packages 'stats' or 'minpack.lm'.")
+
+     m <- length(coef(object))
+     n <- length(object$m$lhs())
+     ## **** R squares ****
+     Adj.R.Square <- (1 - (deviance(object)/((n - m) *
+                                                var(object$m$lhs(),
+                                                    use = "everything"))))
+     Adj.R.Square <- ifelse(is.na(Adj.R.Square) ||
+                                 Adj.R.Square < 0, 0, Adj.R.Square)
+     ## Stein adjusted R square
+     if (m > 2)
+          rho <- ((n - 1)/(n - 4)) * ((n - 2)/(n - 5)) * ((n + 1)/n)
+     else rho <- ((n - 1)/(n - 3)) * ((n - 2)/(n - 4)) * ((n + 1)/n)
+     rho <- 1 - rho * (1 - Adj.R.Square)
+     rho = ifelse(is.na(rho) | rho < 0, 0, rho)
+     res <- data.frame(Adj.R.Square = Adj.R.Square, rho = rho,
+                       AIC = AICmodel(object), BIC = BICmodel(object))
+     return(res)
+}
